6be0a7c84da81c29ea691c544c013de8f83cbe64
ekrsw <eisuke.koresawa@gmail.com>
2022-10-01 10:58:08 +0900

    NewUser

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+	Age  int
+}
+
+func NewUser(name string, age int) *User {
+	return &User{Name: name, Age: age}
+}
+
 func main() {
-	switch n := 3; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-	default:
-		fmt.Println("I don't know.")
-	}
+	user := NewUser("Mike", 18)
+	fmt.Println(user)
 }
