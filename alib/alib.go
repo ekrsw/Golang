e1fac37e58bfcd00d6380e721f5718563a99c05e
ekrsw <eisuke.koresawa@gmail.com>
2022-10-02 10:11:05 +0900

    recovery

--- alib/alib.go
+++
@@ -1,9 +0,0 @@
-package alib
-
-func Average(sl []int) int {
-	total := 0
-	for _, u := range sl {
-		total += u
-	}
-	return int(total / len(sl))
-}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module Golang
 
-go 1.18
+go 1.19
