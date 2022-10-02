02223305815618b4dcdb1a5d1ece550e0ffb8391
ekrsw <eisuke.koresawa@gmail.com>
2022-10-02 09:24:46 +0900

    commit

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"Golang/alib"
-	"fmt"
-)
-
-func IsOne(i int) bool {
-	if i == 1 {
-		return true
-	} else {
-		return false
-	}
-}
+import "fmt"
 
 func main() {
-	fmt.Println(IsOne(1))
-	fmt.Println(IsOne(0))
-
-	sl := []int{1, 2, 3, 4, 5}
-	fmt.Println(alib.Average(sl))
+	fmt.Println("Hello world.")
 }
