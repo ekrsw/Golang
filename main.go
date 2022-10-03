a74d53f6a703027751e420bc8fd7547db65dcf9a
ekrsw <eisuke.koresawa@gmail.com>
2022-10-03 19:06:58 +0900

    20221003

--- main.go
+++ main.go
@@ -2,23 +2,10 @@ package main
 
 import (
 	"fmt"
-	"flag"
+	"strconv"
 )
 
 func main() {
-	var (
-		max int
-		msg string
-		x bool
-	)
-
-	flag.IntVar(&max, "n", 64, "max max max")
-	flag.StringVar(&msg, "m", "", "msg msg msg")
-	flag.BoolVar(&x, "x", false, "x x x")
-
-	flag.Parse()
-
-	fmt.Println(max)
-	fmt.Println(msg)
-	fmt.Println(x)
+	i := strconv.Itoa(100)
+	fmt.Printf("%v, %T\n", i, i)
 }
